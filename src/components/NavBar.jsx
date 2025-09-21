d821909d5a8d16f5ec435e3117e0f647cc011061
Mohit <mohitt.tech@gmail.com>
2025-09-21 20:50:03 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,12 +3,21 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="bg-blue-600 text-white p-4 flex justify-between items-center">
-      <h1 className="ext-xl sm:text-2xl md:text-3xl font-bold">TalkIn</h1>
+    <nav className="bg-blue-600 text-white py-1 px-4 flex justify-between items-center">
+      {/* Logo */}
+      <h1 className="text-xl sm:text-2xl md:text-3xl font-bold">
+        <img
+          src="/images/favicon.png"
+          alt="Logo"
+          className="w-28 h-28 sm:w-32 sm:h-32"
+        />
+      </h1>
+
+      {/* Links */}
       <div className="space-x-4">
         <Link to="/" className="hover:underline">Home</Link>
-        <Link to="/student" className="hover:underline">About</Link>
-        <Link to="/teacher" className="hover:underline">Help</Link>
+        <Link to="/about" className="hover:underline">About</Link>
+        <Link to="/help" className="hover:underline">Help</Link>
       </div>
     </nav>
   );
