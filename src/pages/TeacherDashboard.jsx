75703865a17e1f8bb8d3a6d7bf76718f3cff07f8
Mohit <mohitt.tech@gmail.com>
2025-09-22 20:55:02 +0530

    Create TeacherDashboard.jsx

---
+++ src/pages/TeacherDashboard.jsx
@@ -0,0 +1,10 @@
+// src/pages/TeacherDashboard.jsx
+export default function TeacherDashboard() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold text-blue-600">
+        Welcome to Teacher Dashboard 📘
+      </h1>
+    </div>
+  );
+}
