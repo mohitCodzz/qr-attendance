8a19e62abf5d8d92eec63b0279fea500e88f8502
Mohit <mohitt.tech@gmail.com>
2025-09-22 20:55:06 +0530

    Create StudentDashboard.jsx

---
+++ src/pages/StudentDashboard.jsx
@@ -0,0 +1,10 @@
+// src/pages/StudentDashboard.jsx
+export default function StudentDashboard() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold text-green-600">
+        Welcome to Student Dashboard 🎓
+      </h1>
+    </div>
+  );
+}
