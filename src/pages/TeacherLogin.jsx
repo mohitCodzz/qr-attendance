6c4dad8308753231cfb1fe3a4720c1c72089bf31
Mohit <mohitt.tech@gmail.com>
2025-09-22 12:26:42 +0530

    Update TeacherLogin.jsx

--- src/pages/TeacherLogin.jsx
+++ src/pages/TeacherLogin.jsx
@@ -1,10 +1,10 @@
 // src/pages/TeacherLogin.jsx
-import AttendanceList from "../components/AttendanceList";
+import AttendanceList from "../components/TeacherLogin";
 
 export default function TeacherLogin() {
   return (
     <div className="p-8 flex flex-col items-center">
-      <h1 className="text-3xl font-bold mb-4">Teacher Dashboard</h1>
+      <h1 className="text-3xl font-bold mb-4">Welcome back</h1>
       <AttendanceList />
     </div>
   );
