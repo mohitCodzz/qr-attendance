5ac265158ba0d6050df3ccb21ae6c2dd86b64a3b
Mohit <mohitt.tech@gmail.com>
2025-09-23 18:04:20 +0530

    Update StudentDashboard.jsx

--- src/pages/StudentDashboard.jsx
+++ src/pages/StudentDashboard.jsx
@@ -25,18 +25,30 @@ const AttendanceForm = () => {
 
   return (
     <div className="bg-slate-100 min-h-screen flex flex-col items-center justify-center font-sans px-4 py-8 relative">
-     {/* User id and profile pic */}
-        <div className="absolute top-4 right-4 flex items-center gap-2 bg-white rounded-full p-2 shadow-md">
-          <img
-            src="../images/profile.png"
-            alt="Student Profile"
-            className="w-10 h-10 rounded-full border-2 border-white"
-          />
-          <div className="flex flex-col text-sm pr-2">
-            <p className="text-gray-900 font-bold">Mohit Jaryal</p>
-            <p className="text-gray-500 text-xs">ID: student001</p>
-          </div>
+      {/* User id and profile pic */}
+      <div className="absolute top-4 right-4 flex items-center gap-2 bg-white rounded-full p-2 shadow-md">
+        <img
+          src="../images/profile.png"
+          alt="Student Profile"
+          className="w-10 h-10 rounded-full border-2 border-white"
+        />
+        <div className="flex flex-col text-sm pr-2">
+          <p className="text-gray-900 font-bold">Mohit Jaryal</p>
+          <p className="text-gray-500 text-xs">ID: student001</p>
         </div>
+      </div>
+
+      {/* Welcome  */}
+      <div className="text-center mb-6">
+        <h1 className="text-4xl sm:text-5xl font-extrabold text-gray-800">
+          Welcome back, <span className="text-blue-600">Mohit!</span> 👋
+        </h1>
+        <p className="text-md sm:text-lg text-gray-600 mt-2">
+  {`Hey ${"Mohit"}, scan this QR for marking attendance in your subject: `}
+  <span className="font-semibold text-blue-600">DCCN</span>
+</p>
+
+      </div>
 
       {/* Main Content */}
       <main className="flex flex-col items-center justify-center flex-grow px-4 py-8">
